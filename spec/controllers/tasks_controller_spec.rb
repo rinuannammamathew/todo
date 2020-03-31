54edd050dfa4bb09711ecd1e37d14b17aac4504d
Rinu Annamma Mathew <rinuannammamathew@gmail.com>
2020-03-31 21:27:07 +0000

    added form submittion

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -5,16 +5,10 @@ RSpec.describe TasksController, type: :controller do
     it "should list the tasks in the database" do
       task1 = FactoryBot.create(:task)
       task2 = FactoryBot.create(:task)
-      task1.update_attributes(title: "Something else")
       get :index
-      expect(response).to have_http_status :success
+      assert_response :success
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(2)
-      response_ids = []
-      response_value.each do |task|
-        response_ids << task["id"]
-      end
-      expect(response_ids).to eq([task1.id, task2.id])
     end
   end
 
@@ -27,8 +21,9 @@ RSpec.describe TasksController, type: :controller do
       expect(task.done).to eq(true)
     end
   end
-  describe "task#create" do
-    it "should allow tasks to be created" do
+
+  describe "tasks#create" do
+    it "should allow new tasks to be created" do
       post :create, params: {task: {title: "Fix things"}}
       expect(response).to have_http_status(:success)
       response_value = ActiveSupport::JSON.decode(@response.body)
@@ -36,4 +31,4 @@ RSpec.describe TasksController, type: :controller do
       expect(Task.last.title).to eq("Fix things")
     end
   end
-end 
+end
